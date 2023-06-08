535c7b1a4ad40b772f8dffdb943d0bd3bc145953
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-08 21:26:33 +0500

    Ссылки переделаны в Link

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import './navbar.css'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <nav className='nav'>
       <ul>
         <li>
-          <a href='#!'>Posts</a>
+          <Link to='/'>Posts</Link>
         </li>
         <li>
           <a href='#!'>About Me</a>
