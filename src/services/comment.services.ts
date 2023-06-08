5d5ea4233e98c57febb42b2e6c4a75f928733c49
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-08 21:27:57 +0500

    Сервис с методами для работы с комментариями

--- src/services/comment.services.ts
+++ src/services/comment.services.ts
@@ -1,1 +1,12 @@
 import axios from 'axios'
+
+import { IComment } from '../types/comment.interface'
+import { url } from '../constants'
+
+export const CommentsServices = {
+  async getPostComments(id: number) {
+    const response = await axios.get<IComment[]>(`${url}/posts/${id}/comments`)
+
+    return response.data
+  },
+}
