6ff644863a44bbd4fb28f6d78bbf5e3a516f65bf
«BolotnikovMS» <bolotnikovms@yandex.ru>
2023-06-11 00:55:19 +0500

    Страница с выводом карточек пользователей

--- src/views/Users/UsersPage.tsx
+++ src/views/Users/UsersPage.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 
+import './users.css'
+import { UserCard } from './components/userCard/UserCard'
+import { useUsers } from '../../hooks/useUsers'
+import { Error } from '../../components/error/Error'
+import { Loader } from '../../components/loader/Loader'
+
 export const UsersPage = () => {
-  return <div>UsersPage</div>
+  const { users, isLoading, error } = useUsers()
+
+  return (
+    <section className='users'>
+      <div className='user__content'>
+        <h2 className='title'>All users</h2>
+        {error ? (
+          <Error message={error} />
+        ) : isLoading ? (
+          <Loader />
+        ) : (
+          <div className='users__cards'>
+            {users?.map(userItem => (
+              <UserCard key={userItem.id} user={userItem} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
