bb38aa10ada7f2b36839cf49875df12d48a24a71
«BolotnikovMS» <bolotnikovms@yandex.ru>
2023-06-11 01:09:53 +0500

    Добавлена иконка

--- src/views/Users/components/userCard/UserCard.tsx
+++ src/views/Users/components/userCard/UserCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './user-card.css'
+import userIcon from './../../../../assets/img/icons/user.svg'
 import { Link } from 'react-router-dom'
 import { IUser } from '../../../../types/user.interface'
 
@@ -12,9 +13,12 @@ export const UserCard = ({ user }: PropsUserCard) => {
   return (
     <div className='user__card'>
       <Link to={`/users/${user.id}`} className='user__link' />
-      <div className='user__card-body'>{user.name}</div>
+      <div className='user__card-body'>
+        <img src={userIcon} alt='User avatar' className='user__card-img' />
+        <span className='user__card-text'>{user.name}</span>
+      </div>
       <hr className='line' />
-      <div className='user__card-footer'>{user.email}</div>
+      <div className='user__card-footer'>Email: {user.email}</div>
     </div>
   )
 }
