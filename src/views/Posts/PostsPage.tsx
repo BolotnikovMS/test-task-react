4953aa6e7524bb17f77df4965d1daccce259088b
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-10 15:42:54 +0500

    Правки

--- src/views/Posts/PostsPage.tsx
+++ src/views/Posts/PostsPage.tsx
@@ -10,22 +10,21 @@ export const PostsPage = () => {
   const { posts, isLoading, error } = usePosts()
 
   return (
-    <>
-      {error ? (
-        <Error message={error} />
-      ) : (
-        <section className='posts'>
-          <div className='posts__content'>
-            <h2 className='title'>All posts</h2>
-            {isLoading && <Loader />}
-            <div className='posts__cards'>
-              {posts.map(post => (
-                <PostCard key={post.id} post={post} />
-              ))}
-            </div>
+    <section className='posts'>
+      <div className='posts__content'>
+        <h2 className='title'>All posts</h2>
+        {error ? (
+          <Error message={error} />
+        ) : isLoading ? (
+          <Loader />
+        ) : (
+          <div className='posts__cards'>
+            {posts.map(post => (
+              <PostCard key={post.id} post={post} />
+            ))}
           </div>
-        </section>
-      )}
-    </>
+        )}
+      </div>
+    </section>
   )
 }
