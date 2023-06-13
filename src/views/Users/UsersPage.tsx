549d58f874efeaf7e83825fd776c861a6d5e502b
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-13 21:06:03 +0500

    Добавлена пагинация

--- src/views/Users/UsersPage.tsx
+++ src/views/Users/UsersPage.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 import './users.css'
 import { UserCard } from './components/userCard/UserCard'
 import { useUsers } from '../../hooks/useUsers'
 import { Error } from '../../components/error/Error'
 import { Loader } from '../../components/loader/Loader'
+import { Pagination } from '../../components/pagination/Pagination'
+
+const PageSize = 5
 
 export const UsersPage = () => {
   const { users, isLoading, error } = useUsers()
+  const [currentPage, setCurrentPage] = useState(1)
+  const currentUsersList = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize
+    const lastPageIndex = firstPageIndex + PageSize
+    return users?.slice(firstPageIndex, lastPageIndex)
+  }, [currentPage, users])
 
   return (
     <section className='users'>
@@ -18,11 +27,19 @@ export const UsersPage = () => {
         ) : isLoading ? (
           <Loader />
         ) : (
-          <div className='users__cards'>
-            {users?.map(userItem => (
-              <UserCard key={userItem.id} user={userItem} />
-            ))}
-          </div>
+          <>
+            <div className='users__cards'>
+              {currentUsersList?.map(userItem => (
+                <UserCard key={userItem.id} user={userItem} />
+              ))}
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              totalCount={users?.length}
+              pageSize={PageSize}
+              onPageChange={page => setCurrentPage(page)}
+            />
+          </>
         )}
       </div>
     </section>
