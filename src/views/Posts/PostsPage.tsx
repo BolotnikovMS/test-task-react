76283b77c614c5ecb6ff99941547b8ae9be01c93
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-13 22:04:57 +0500

    Добавлена пагинация

--- src/views/Posts/PostsPage.tsx
+++ src/views/Posts/PostsPage.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 import './posts.css'
 import { PostCard } from './components/postCard/PostCard'
 import { Loader } from '../../components/loader/Loader'
 import { Error } from '../../components/error/Error'
 import { usePosts } from '../../hooks/usePosts'
+import { Pagination } from '../../components/pagination/Pagination'
+
+const PageSize = 10
 
 export const PostsPage = () => {
   const { posts, isLoading, error } = usePosts()
+  const [currentPage, setCurrentPage] = useState(1)
+  const currentPosts = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize
+    const lastPageIndex = firstPageIndex + PageSize
+    return posts?.slice(firstPageIndex, lastPageIndex)
+  }, [currentPage, posts])
 
   return (
     <section className='posts'>
@@ -18,11 +27,19 @@ export const PostsPage = () => {
         ) : isLoading ? (
           <Loader />
         ) : (
-          <div className='posts__cards'>
-            {posts.map(post => (
-              <PostCard key={post.id} post={post} />
-            ))}
-          </div>
+          <>
+            <div className='posts__cards'>
+              {currentPosts.map(post => (
+                <PostCard key={post.id} post={post} />
+              ))}
+              <Pagination
+                currentPage={currentPage}
+                totalCount={posts?.length}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}
+              />
+            </div>
+          </>
         )}
       </div>
     </section>
