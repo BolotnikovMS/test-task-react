d82380b9a2e0c21dd494ac4b8870c5e824866b7b
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-13 21:05:27 +0500

    Компонент пагинации

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import './pagination.css'
+import { usePagination, DOTS, PropsPaginationHook } from '../../hooks/usePagination'
+
+interface PropsPaginationComponent extends PropsPaginationHook {
+  onPageChange: (arg: number) => void
+  className?: string
+}
+
+export const Pagination = ({
+  onPageChange,
+  totalCount,
+  siblingCount,
+  currentPage,
+  pageSize,
+  className,
+}: PropsPaginationComponent) => {
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  })
+
+  if (currentPage === 0 || paginationRange?.length === undefined || paginationRange.length < 2) {
+    return null
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1)
+  }
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1)
+  }
+
+  const lastPage = paginationRange[paginationRange.length - 1]
+  return (
+    <ul className={`pagination ${className}`}>
+      <li className={`pagination-item ${currentPage === 1 ? 'disabled' : ''}`} onClick={onPrevious}>
+        {'<<'}
+      </li>
+      {paginationRange.map(pageNumber => {
+        if (pageNumber === DOTS) {
+          return <li className='pagination-item dots'>&#8230;</li>
+        }
+
+        return (
+          <li
+            className={`pagination-item ${pageNumber === currentPage ? 'selected' : ''}`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        )
+      })}
+      <li
+        className={`pagination-item ${currentPage === lastPage ? 'disabled' : ''}`}
+        onClick={onNext}
+      >
+        {'>>'}
+      </li>
+    </ul>
+  )
+}
