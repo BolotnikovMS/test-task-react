aae628a4044bdf16a62de206b29abe0aefeafb44
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-18 16:35:37 +0500

    Добавлен импорт компонента иконки

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './button.css'
+import { Icon } from '../icon/Icon'
 
 enum ButtonTypes {
   Button = 'button',
@@ -12,14 +13,17 @@ interface PropsButton {
   name: string
   typeBtn?: ButtonTypes
   classBtn?: string
+  handlerEvent?: () => void
+  iconName?: string
 }
 
-export const Button = ({ name, typeBtn, classBtn }: PropsButton) => {
+export const Button = ({ name, typeBtn, iconName, classBtn, handlerEvent }: PropsButton) => {
   const styleBtn = `btn ${classBtn ? classBtn : ''}`
 
   return (
-    <button type={typeBtn} className={styleBtn}>
+    <button type={typeBtn} className={styleBtn} onClick={handlerEvent}>
       {name}
+      {iconName ? <Icon nameIcon={iconName} /> : null}
     </button>
   )
 }
