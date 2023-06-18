1c0a231b80fbbcec647d532f3bbd42ad7279ce6f
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-18 16:37:42 +0500

    Добавлена искусственная задержка

--- src/services/comment.services.ts
+++ src/services/comment.services.ts
@@ -2,10 +2,13 @@ import axios from 'axios'
 
 import { IComment } from '../types/comment.interface'
 import { url } from '../constants'
+import { delaying } from '../helpers/delaying';
 
 export const CommentsServices = {
   async getPostComments(id: number) {
     const response = await axios.get<IComment[]>(`${url}/posts/${id}/comments`)
+
+    await delaying(500)
 
     return response.data
   },
--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -2,11 +2,14 @@ import axios from 'axios'
 
 import { IPost } from '../types/post.interface'
 import { url } from '../constants'
+import { delaying } from '../helpers/delaying'
 
 export const PostServices = {
   async getAllPosts() {
     // const response = await axios.get<IPost[]>(`${url}/posts?_limit=10`)
     const response = await axios.get<IPost[]>(`${url}/posts`)
+
+    await delaying(500)
 
     return response.data
   },
