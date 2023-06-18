5f6ee0d6c4ad01ff2c0d713f97b08f935b5172b0
BolotnikovMS <bolotnikovms@yandex.ru>
2023-06-18 16:39:34 +0500

    Добавлен компонент иконки

--- src/views/Users/components/userCard/UserCard.tsx
+++ src/views/Users/components/userCard/UserCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import './user-card.css'
-import userIcon from './../../../../assets/img/icons/user.svg'
 import { Link } from 'react-router-dom'
 import { IUser } from '../../../../types/user.interface'
+import { Icon } from '../../../../components/icon/Icon'
 
 interface PropsUserCard {
   user: IUser
@@ -14,7 +14,7 @@ export const UserCard = ({ user }: PropsUserCard) => {
     <div className='user__card'>
       <Link to={`/users/${user.id}`} className='user__link' />
       <div className='user__card-body'>
-        <img src={userIcon} alt='User avatar' className='user__card-img' />
+        <Icon nameIcon='user.svg' classIcon='icon_wh-25' altText='Icon user' />
         <span className='user__card-text'>{user.name}</span>
       </div>
       <hr className='line' />
