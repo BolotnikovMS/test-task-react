17bcbe2a8b1a4f67e053bcebeaee24f88ab22cf1
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-17 20:52:28 +0500

    refactor: :art: Правки

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-
 import './icon.css'
+
+import React from 'react'
 
 interface PropsIcon {
   nameIcon: string
   classIcon?: string
-  altText?: string
+  altTextIcon?: string
 }
 
-export const Icon = ({ nameIcon, classIcon, altText }: PropsIcon) => {
+export const Icon = ({ nameIcon, classIcon, altTextIcon }: PropsIcon) => {
   const iconPath = `./../../../public/img/icons/${nameIcon}`
 
   return (
     <img
       src={iconPath}
       className={`icon ${classIcon ? classIcon : ''}`}
-      alt={altText ? altText : nameIcon}
+      alt={altTextIcon ? altTextIcon : nameIcon}
     />
   )
 }
