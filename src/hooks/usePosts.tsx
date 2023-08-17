f54f5318b2380e522527d6c70a937a59b18d8a9d
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-17 20:54:07 +0500

    feat: :sparkles: Правки, сортировка постов
    Добавлена функция сортировки постов

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useMemo, useState } from 'react'
 
+import { AxiosError } from 'axios'
 import { IPost } from '../types/post.interface'
 import { PostServices } from '../services/post.services'
-import { AxiosError } from 'axios'
 
 export const usePosts = () => {
   const [posts, setPosts] = useState<IPost[]>([])
   const [parameterPostsSearch, setParameterPostsSearch] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
+  const [sortedPosts, setSortedPosts] = useState<IPost[]>(posts)
 
   const fetchPosts = async () => {
     try {
@@ -18,6 +19,7 @@ export const usePosts = () => {
       const posts = await PostServices.getAllPosts()
 
       setPosts(posts)
+      setSortedPosts(posts)
       setIsLoading(false)
     } catch (error: unknown) {
       const e = error as AxiosError
@@ -32,12 +34,29 @@ export const usePosts = () => {
   }, [])
 
   const getSearchList = () => {
-    if (!parameterPostsSearch) return posts
+    if (!parameterPostsSearch) return sortedPosts
 
-    return posts.filter((post: IPost) => post.title.includes(parameterPostsSearch))
+    return sortedPosts.filter((post: IPost) => post.title.includes(parameterPostsSearch))
   }
 
-  const searchPosts = useMemo(getSearchList, [parameterPostsSearch, posts])
+  const sortPosts = (pattern: string) => {
+    let direction: number
+    pattern === 'asc' ? direction = 1 : pattern === 'desc' ? direction = -1 : 0
+    
+    const sorted = [...posts].sort((a: IPost, b: IPost) => {
+      if (a.title === b.title) return 0
 
-  return { posts, isLoading, error, setParameterPostsSearch, searchPosts }
+      return a.title > b.title ? direction : direction * -1
+    })
+    setSortedPosts(sorted)
+  }
+
+  const resetSort = () => {
+    setSortedPosts(posts)
+  } 
+
+  
+  const searchPosts = useMemo(getSearchList, [parameterPostsSearch, sortedPosts])
+
+  return { isLoading, error, setParameterPostsSearch, searchPosts, sortPosts, resetSort }
 }
