b98fba6a7b92cb7f822920752dc201cbef10438f
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-24 18:33:30 +0500

    feat: Добавлена ссылка для перехода на главную страницу, также подготовлен блок для вывода постов пользователя

--- src/views/Users/UserInfoPage.tsx
+++ src/views/Users/UserInfoPage.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
 import './users.css'
-import { useUsers } from '../../hooks/useUsers'
-import { Loader } from '../../components/loader/Loader'
+
 import { Error } from '../../components/error/Error'
+import { Icon } from '../../components/icon/Icon'
+import { Link } from 'react-router-dom'
+import { Loader } from '../../components/loader/Loader'
+import React from 'react'
 import { UserGroup } from './components/userGroup/UserGroup'
+import { useUsers } from '../../hooks/useUsers'
 
 export const UserInfoPage = () => {
   const { user, isLoading, error } = useUsers()
@@ -18,14 +20,25 @@ export const UserInfoPage = () => {
         ) : error ? (
           <Error message={error} />
         ) : (
-          <div className='user__info'>
-            <UserGroup groupName='Name' value={user?.name} />
-            <UserGroup groupName='Username' value={user?.username} />
-            <UserGroup groupName='Email' value={user?.email} />
-            <UserGroup groupName='Phone' value={user?.phone} />
-            <UserGroup groupName='Website' value={user?.website} />
-            <UserGroup groupName='Company' value={user?.company.name} />
-          </div>
+          <>
+            <div className='user__info'>
+              <UserGroup groupName='Name' value={user?.name} />
+              <UserGroup groupName='Username' value={user?.username} />
+              <UserGroup groupName='Email' value={user?.email} />
+              <UserGroup groupName='Phone' value={user?.phone} />
+              <UserGroup groupName='Website' value={user?.website} />
+              <UserGroup groupName='Company' value={user?.company.name} />
+            </div>
+            <div className="user__posts">
+              <h3>User posts</h3>
+            </div>
+            <div className="users__btns">
+              <Link to={'/'} className='link'>
+                <Icon nameIcon='arrow-left.svg'/>
+                Back to homepage
+              </Link>
+            </div>
+          </>
         )}
       </div>
     </section>
