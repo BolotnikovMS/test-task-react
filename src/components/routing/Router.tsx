721e59c9b0ec68c554a128d4d54211ee67f2d4b0
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-24 18:16:30 +0500

    feat: :sparkles: Добавлен роут
    Добавлен роут ведущий на страницу с подробным просмотром поста

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import { Header } from '../header/Header'
+import { PostInfoPage } from '../../views/Posts/PostInfoPage'
 import { PostsPage } from '../../views/Posts/PostsPage'
+import React from 'react'
 import { UserInfoPage } from '../../views/Users/UserInfoPage'
 import { UsersPage } from '../../views/Users/UsersPage'
 
@@ -12,6 +13,7 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route element={<PostsPage />} path='/' />
+        <Route element={<PostInfoPage />} path='/posts/:id' />
         <Route element={<UserInfoPage />} path='/users/:id' />
         <Route element={<UsersPage />} path='/users' />
       </Routes>
