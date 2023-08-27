98b2cb3f0fe8ab2b61c797e3514ff20b8f64caaf
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-27 21:43:22 +0500

    feat: :sparkles: Добавлен провайдер react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
 import './styles/reset.css'
 import './styles/main.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Router } from './components/routing/Router'
 
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div className='container'>
-    <Router />
-  </div>
+  <QueryClientProvider client={client}>
+    <div className='container'>
+      <Router />
+    </div>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
