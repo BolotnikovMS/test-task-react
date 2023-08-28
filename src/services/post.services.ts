5d70c7ebc6019d77397a558803703fa18673a899
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-28 21:38:49 +0500

    feat: :sparkles: Получение постов с сортировкой
    В фун-ю getAllPosts добавлен параметр с паттерном для сортировки

--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -1,19 +1,22 @@
 import { IPost } from '../types/post.interface'
+import { PostPatternSortType } from '../types/posts.types'
 import axios from 'axios'
 import { delaying } from '../helpers/delaying'
 import { url } from '../constants'
 
 export const PostServices = {
-  async getAllPosts() {
+  async getAllPosts(patternSort: PostPatternSortType = 'asc') {
     // const response = await axios.get<IPost[]>(`${url}/posts?_limit=10`)
-    const response = await axios.get<IPost[]>(`${url}/posts`)
+    // https://jsonplaceholder.typicode.com/posts?_sort=title&_order=asc
+    const qs = patternSort === 'asc' ? `?_sort=title&_order=${patternSort}` : `?_sort=title&_order=${patternSort}`
+    const response = await axios.get<IPost[]>(`${url}/posts${qs}`)
 
     await delaying(500)
 
     return response.data
   },
 
-  async getPostById(id: number) {
+  async getPostById(id: string) {
     const response = await axios.get<IPost>(`${url}/posts/${id}`)
 
     return response.data
