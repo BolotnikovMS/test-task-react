1dcafdb9fd715914b9465c3beecf7b45ce033d05
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-28 21:35:12 +0500

    feat: :sparkles: Хук для получения 1 поста
    Кастомный хук для получения одного поста с использованием react query

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,13 @@
+import { PostServices } from '../services/post.services'
+import { useQuery } from '@tanstack/react-query'
+
+export const usePost = (id: string | undefined) => {
+  const { data: post, error, isLoading, isError } = useQuery({
+    queryFn: () => PostServices.getPostById(id || ''),
+    queryKey: ['post', id],
+    staleTime: 1000 * 10,
+    enabled: !!id,
+  })
+
+  return { post, error, isLoading, isError }
+}
