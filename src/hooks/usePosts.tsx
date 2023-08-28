4d911891a04b2fc1ca615f30e6fb2433639ec4b5
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-28 21:36:52 +0500

    refactor: :recycle: Использование react query

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,80 +1,15 @@
-import React, { useEffect, useState } from 'react'
-
-import { AxiosError } from 'axios'
-import { IPost } from '../types/post.interface'
+import { PostPatternSortType } from '../types/posts.types'
 import { PostServices } from '../services/post.services'
-import { useParams } from 'react-router-dom'
+import React from 'react'
+import { useQuery } from '@tanstack/react-query'
 
-export const usePosts = () => {
-  const [posts, setPosts] = useState<IPost[]>([])
-  const [post, setPost] = useState<IPost>()
-  const { id } = useParams()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string | null>(null)
-  const [sortedPosts, setSortedPosts] = useState<IPost[]>(posts)
+export const usePosts = (pattern: PostPatternSortType) => {
+  const { data: posts, error, isError, isLoading } = useQuery({
+    queryFn: () => PostServices.getAllPosts(pattern),
+    queryKey: ['posts', pattern],
+    staleTime: 1000 * 10,
+    keepPreviousData: true,
+  })
 
-  const fetchPosts = async () => {
-    try {
-      setIsLoading(true)
-      setError(null)
-
-      const posts = await PostServices.getAllPosts()
-
-      setPosts(posts)
-      setSortedPosts(posts)
-      setIsLoading(false)
-    } catch (error: unknown) {
-      const e = error as AxiosError
-
-      setIsLoading(false)
-      setError(e.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
-  const fetchPost = async (id: number) => {
-    if (!id) return
-
-    try {
-      setIsLoading(true)
-      setError('')
-
-      const post: IPost = await PostServices.getPostById(id)
-
-      setPost(post)
-      setIsLoading(false)
-    } catch (error: unknown) {
-      const e = error as AxiosError
-
-      setIsLoading(false)
-      setError(e.message)
-    }
-  }
-
-  useEffect(() => {
-    if (!id) return
-    
-    fetchPost(+id)
-  }, [id])
-
-  const sortPosts = (pattern: string) => {
-    let direction: number
-    pattern === 'asc' ? direction = 1 : pattern === 'desc' ? direction = -1 : 0
-    
-    const sorted = [...posts].sort((a: IPost, b: IPost) => {
-      if (a.title === b.title) return 0
-
-      return a.title > b.title ? direction : direction * -1
-    })
-    setSortedPosts(sorted)
-  }
-
-  const resetSort = () => {
-    setSortedPosts(posts)
-  }
-
-  return { posts, post, isLoading, error, sortedPosts, sortPosts, resetSort }
+  return { posts, error, isLoading, isError }
 }
