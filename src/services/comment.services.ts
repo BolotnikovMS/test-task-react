9c59b8b7cc71bc4089357a8b87a66377ef056079
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-29 21:01:57 +0500

    fix: Поправлен тип

--- src/services/comment.services.ts
+++ src/services/comment.services.ts
@@ -1,11 +1,10 @@
-import axios from 'axios'
-
 import { IComment } from '../types/comment.interface'
-import { url } from '../constants'
+import axios from 'axios'
 import { delaying } from '../helpers/delaying';
+import { url } from '../constants'
 
 export const CommentsServices = {
-  async getPostComments(id: number) {
+  async getPostComments(id: string) {
     const response = await axios.get<IComment[]>(`${url}/posts/${id}/comments`)
 
     await delaying(500)
