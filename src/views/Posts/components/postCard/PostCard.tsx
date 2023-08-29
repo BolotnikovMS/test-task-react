8c5170a392b76b6988704500bfbd4115b077d1dc
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-29 21:09:35 +0500

    refactor: Рефакторинг
    Получение комментариев с помощью react query

--- src/views/Posts/components/postCard/PostCard.tsx
+++ src/views/Posts/components/postCard/PostCard.tsx
@@ -1,50 +1,33 @@
 import './post-card.css'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { AxiosError } from 'axios'
 import { Button } from '../../../../components/buttons/Button'
 import { CommentCard } from '../commentCard/CommentCard'
-import { CommentsServices } from '../../../../services/comment.services'
 import { Error } from '../../../../components/error/Error'
-import { IComment } from '../../../../types/comment.interface'
 import { IPost } from '../../../../types/post.interface'
 import { Icon } from '../../../../components/icon/Icon'
 import { Link } from 'react-router-dom'
 import { Loader } from '../../../../components/loader/Loader'
+import { useComments } from '../../../../hooks/useComments'
 
 interface PropsPostCards {
   post: IPost
 }
 
 export const PostCard = ({ post }: PropsPostCards) => {
-  const [comments, setComments] = useState<IComment[]>([])
-  const [show, setShow] = useState(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string | null>(null)
-
-  const fetchCommentsPost = async (idPost: number) => {
-    try {
-      setIsLoading(true)
-      setError(null)
-
-      const response = await CommentsServices.getPostComments(idPost)
-
-      setComments(response)
-      setIsLoading(false)
-    } catch (error) {
-      const e = error as AxiosError
-
-      setIsLoading(false)
-      setError(e.message)
-    }
-  }
+  const [show, setShow] = useState<boolean>(false)
+  const { comments, error, isLoading, isError, refetch } = useComments(String(post.id))
 
   const showComments = () => {
     setShow(!show)
-
-    if (!show) fetchCommentsPost(post.id)
   }
+
+  useEffect(() => {
+    if (show === true) refetch()
+  }, [refetch, show])
+
 
   return (
     <div className='card'>
@@ -67,8 +50,8 @@ export const PostCard = ({ post }: PropsPostCards) => {
         {show ? (
           isLoading ? (
             <Loader />
-          ) : error ? (
-            <Error message={error} />
+          ) : isError ? (
+            <Error message={(error as AxiosError).message} />
           ) : (
             <div className='card__comments'>
               {comments &&
