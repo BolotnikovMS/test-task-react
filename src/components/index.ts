d6c7d995f18c8332d3a7a4147e74410cb800d1a7
BolotnikovMS <bolotnikovms@yandex.ru>
2023-08-31 20:02:09 +0500

    feat: index файл с экспортом компонентов

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import { Button } from './buttons/Button'
+import { Dropdown } from './dropdown/Dropdown'
+import { Error } from './error/Error'
+import { Icon } from './icon/Icon'
+import { InfoMessage } from './infoMessage/InfoMessage'
+import { Loader } from './loader/Loader'
+import { Pagination } from './pagination/Pagination'
+import { Search } from './search/Search'
+
+export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search }
