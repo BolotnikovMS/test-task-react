642309a17de018d7590e5b034db9a02f41583424
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-10 16:44:11 +0500

    perf: Изменена логика компонента

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,67 +1,35 @@
 import './search.css'
 
 import { Button, Icon } from '..'
-import { IPost, IUser } from '../../interfaces'
-import React, { ChangeEvent, useEffect, useMemo, useState } from 'react'
-
-import { Link } from 'react-router-dom'
-
-type KeysObj = 'name' | 'title' | 'body' | 'username'
+import React, { ChangeEvent, Dispatch, SetStateAction } from 'react'
 
 interface PropsSearch {
-  arrayToSearch: IUser[] | IPost[] | undefined
-  keyObjSearch: KeysObj
-  keyObjName: KeysObj
-  linkPath?: string
+  children: React.ReactNode
+  searchQuery: string
+  setSearchQuery: Dispatch<SetStateAction<string>>
+  onChange: (value: string) => void
 }
 
-export const Search = ({ arrayToSearch, keyObjSearch, keyObjName, linkPath }: PropsSearch) => {
-  const [desiredValue, setDesiredValue] = useState<string>('')
-  
-  const handleChanges = ({target}: ChangeEvent<HTMLInputElement>) => {
-    setDesiredValue(target.value)
-  }
-  
+export const Search = ({ children, searchQuery, setSearchQuery, onChange }: PropsSearch) => {
   const handleClickReset = () => {
-    setDesiredValue('')
+    setSearchQuery('')
   }
-
-  const getSearchResult = () => {
-    if (!desiredValue) return
-
-    return arrayToSearch?.filter(item => item[keyObjSearch].toLocaleLowerCase().includes(desiredValue.toLocaleLowerCase()))
-  }
-
-  const searchResult = useMemo(getSearchResult, [desiredValue, arrayToSearch, keyObjSearch])
 
   return (
     <div className='search'>
       <div className='search__group'>
         <input
           type='text'
-          value={desiredValue}
+          value={searchQuery}
           className='search__input'
           placeholder='Search...'
-          onChange={handleChanges}
+          onChange={({target}: ChangeEvent<HTMLInputElement>) => onChange(target.value)}
         />
         <Button classBtn='search__btn' onClick={handleClickReset}>
           <Icon nameIcon='close.svg' classIcon='icon_wh-21' />
         </Button>
       </div>
-      <ul className="search__list">
-        {
-          desiredValue ? (
-            searchResult?.length ? (
-              searchResult.map(result => 
-                <li key={result.id} className='search__list-item'>
-                  <Link to={linkPath ? `${linkPath}${result.id}` : `${result.id}`}>{result[keyObjName].substring(30, -10) + '...'}</Link>
-                </li>)
-            ) : (
-              <li className="search__list-item">No result...</li>
-            )
-          ) : null
-        }
-      </ul>
+      {children}
     </div>
   )
 }
