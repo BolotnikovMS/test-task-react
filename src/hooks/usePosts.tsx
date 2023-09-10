467fc2d3bd503a73b4c967076facdb7a9e2a76ae
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-10 16:46:09 +0500

    refactor: Правки

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -5,7 +5,7 @@ import { useQuery } from '@tanstack/react-query'
 
 export const usePosts = (pattern: PostPatternSortType) => {
   const { data: posts, error, isError, isLoading } = useQuery({
-    queryFn: () => PostServices.getAllPosts(pattern),
+    queryFn: () => PostServices.getAllPosts({patternOrder: pattern}),
     queryKey: ['posts', pattern],
     staleTime: 1000 * 10,
     keepPreviousData: true,
