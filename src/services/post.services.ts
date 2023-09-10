62829efdd204fa0c12126ca23e59f485b2d91fa9
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-10 16:47:20 +0500

    refactor: Добавлен интерфейс для параметров

--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -4,12 +4,20 @@ import axios from 'axios'
 import { delaying } from '../helpers/delaying'
 import { url } from '../constants'
 
+interface IParamsPostsServices {
+  patternSort?: string
+  patternOrder?: PostPatternSortType 
+  searchString?: string
+}
+
 export const PostServices = {
-  async getAllPosts(patternSort: PostPatternSortType = 'asc') {
+  async getAllPosts({ patternSort, patternOrder, searchString }: IParamsPostsServices) {
     // const response = await axios.get<IPost[]>(`${url}/posts?_limit=10`)
     // https://jsonplaceholder.typicode.com/posts?_sort=title&_order=asc
-    const qs = patternSort === 'asc' ? `?_sort=title&_order=${patternSort}` : `?_sort=title&_order=${patternSort}`
-    const response = await axios.get<IPost[]>(`${url}/posts${qs}`)
+    // const qs = patternSort === 'asc' ? `?_sort=title&_order=${patternSort}` : `?_sort=title&_order=${patternSort}`
+    const response = await axios.get<IPost[]>(`${url}/posts`, {
+      params: { _sort: 'title', _order: patternOrder, q: searchString }
+    })
 
     await delaying(500)
 
