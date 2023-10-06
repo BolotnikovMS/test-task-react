44ea055842ea29095617eed9bf9c84c256b7ec36
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-06 19:19:46 +0500

    refactor: Файл с удобным экспортом хуков

---
+++ src/hooks/index.ts
@@ -0,0 +1,10 @@
+import { useComments } from './useComments'
+import { usePagination } from './usePagination'
+import { usePost } from './usePost'
+import { usePosts } from './usePosts'
+import { usePostsUser } from './usePostsUser'
+import { useTitle } from './useTitle'
+import { useUser } from './useUser'
+import { useUsers } from './useUsers'
+
+export { useComments, usePagination, usePosts, usePost, usePostsUser, useTitle, useUser, useUsers }
