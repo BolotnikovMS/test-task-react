281f5fdb74849c32b1007c3fa53dce16d0044408
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-06 19:20:14 +0500

    refactor: Файл с удобным экспортом страниц

---
+++ src/views/index.ts
@@ -0,0 +1,7 @@
+import { NotFound } from './NotFound/NotFound'
+import { PostInfoPage } from './Posts/PostInfoPage'
+import { PostsPage } from './Posts/PostsPage'
+import { UserInfoPage } from './Users/UserInfoPage'
+import { UsersPage } from './Users/UsersPage'
+
+export { NotFound, PostsPage, PostInfoPage, UsersPage, UserInfoPage }
