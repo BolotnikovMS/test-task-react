67b9afe10d367ffee56912ba5e710b6d9f78e2a1
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-11 18:58:25 +0500

    feat: Добавлен маршрут для работы с формой добавления нового поста

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotFound, PostInfoPage, PostsPage, UserInfoPage, UsersPage } from '../../views'
 
 import { Header } from '../header/Header'
+import { NewPost } from '../../views/Posts/NewPost'
 import React from 'react'
 
 export const Router = () => {
@@ -10,6 +11,7 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route element={<PostsPage />} path='/' />
+        <Route element={<NewPost />} path='/new' />
         <Route element={<PostInfoPage />} path='/posts/:id' />
         <Route element={<UserInfoPage />} path='/users/:id' />
         <Route element={<UsersPage />} path='/users' />
