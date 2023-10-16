101e803ecfabb440fd219884b3b6a8f73fe41667
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-16 21:35:45 +0500

    feat: Добавлены компоненты для экспорта

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,15 @@
 import { Button } from './buttons/Button'
 import { Dropdown } from './dropdown/Dropdown'
 import { Error } from './error/Error'
+import { FormGroup } from './forms/formGroup/FormGroup'
 import { Icon } from './icon/Icon'
 import { InfoMessage } from './infoMessage/InfoMessage'
 import { Loader } from './loader/Loader'
 import { Pagination } from './pagination/Pagination'
 import { Search } from './search/Search'
 import { SearchList } from './search/SearchList'
+import { TextInput } from './forms/input/TextInput'
+import { Textarea } from './forms/textarea/Textarea'
+import { ValidationMessage } from './forms/errorMessage/ValidationMessage'
 
-export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList }
+export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, ValidationMessage, TextInput, Textarea, FormGroup }
