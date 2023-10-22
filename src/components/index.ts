dbd7ca705655738a7e9624f0ab35378a717f43e1
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-22 19:03:23 +0500

    refactor: Добавлен импорт Header, Layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,10 @@ import { Button } from './buttons/Button'
 import { Dropdown } from './dropdown/Dropdown'
 import { Error } from './error/Error'
 import { FormGroup } from './forms/formGroup/FormGroup'
+import { Header } from './header/Header'
 import { Icon } from './icon/Icon'
 import { InfoMessage } from './infoMessage/InfoMessage'
+import { Layout } from './layout/Layout';
 import { Loader } from './loader/Loader'
 import { Pagination } from './pagination/Pagination'
 import { Search } from './search/Search'
@@ -12,4 +14,4 @@ import { TextInput } from './forms/input/TextInput'
 import { Textarea } from './forms/textarea/Textarea'
 import { ValidationMessage } from './forms/errorMessage/ValidationMessage'
 
-export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, ValidationMessage, TextInput, Textarea, FormGroup }
+export { Layout, Header, Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, ValidationMessage, TextInput, Textarea, FormGroup }
