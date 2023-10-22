85cd2010c1d5819d018fd33fc1ac50dd08cdeb99
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-22 17:37:52 +0500

    refactor: Рефакторинг

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotFound, PostInfoPage, PostsPage, UserInfoPage, UsersPage } from '../../views'
+import { Route, Routes } from 'react-router-dom'
 
 import { Layout } from '../layout/Layout'
 import { NewPost } from '../../views/Posts/NewPost'
@@ -9,18 +9,16 @@ import { TestText } from '../../views/Test/TestText'
 
 export const Router = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL} >
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<PostsPage />} />
-          <Route path='new' element={<NewPost />} />
-          <Route path='posts/:id' element={<PostInfoPage />} />
-          <Route path='users/:id' element={<UserInfoPage />} />
-          <Route path='users' element={<UsersPage />} />
-          <Route path='tests' element={<TestPage children={<TestText/>} />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostsPage />} />
+        <Route path='new' element={<NewPost />} />
+        <Route path='posts/:id' element={<PostInfoPage />} />
+        <Route path='users/:id' element={<UserInfoPage />} />
+        <Route path='users' element={<UsersPage />} />
+        <Route path='tests' element={<TestPage children={<TestText/>} />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles/main.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App'
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -20,7 +21,9 @@ const client = new QueryClient(
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={client}>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL} >
+      <App />
+    </BrowserRouter>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
