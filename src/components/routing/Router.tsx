8f5462760e9b435fba93061d41ecac03fc497454
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-22 17:34:07 +0500

    feat: Использован компонент Layout

--- src/components/routing/Router.tsx
+++ src/components/routing/Router.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotFound, PostInfoPage, PostsPage, UserInfoPage, UsersPage } from '../../views'
 
-import { Header } from '../header/Header'
+import { Layout } from '../layout/Layout'
 import { NewPost } from '../../views/Posts/NewPost'
 import React from 'react'
+import { TestPage } from '../../views/Test/TestPage'
+import { TestText } from '../../views/Test/TestText'
 
 export const Router = () => {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL} >
-      <Header />
       <Routes>
-        <Route element={<PostsPage />} path='/' />
-        <Route element={<NewPost />} path='/new' />
-        <Route element={<PostInfoPage />} path='/posts/:id' />
-        <Route element={<UserInfoPage />} path='/users/:id' />
-        <Route element={<UsersPage />} path='/users' />
-        <Route element={<NotFound />} path='*' />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<PostsPage />} />
+          <Route path='new' element={<NewPost />} />
+          <Route path='posts/:id' element={<PostInfoPage />} />
+          <Route path='users/:id' element={<UserInfoPage />} />
+          <Route path='users' element={<UsersPage />} />
+          <Route path='tests' element={<TestPage children={<TestText/>} />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
