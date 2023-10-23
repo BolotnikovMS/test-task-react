f0cd3fc12c9e0b18b2e1417e9ee504ff6d171129
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-23 20:19:43 +0500

    refactor: Убран импорт

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,7 +1,6 @@
-import { NotFound } from './NotFound/NotFound'
 import { PostInfoPage } from './Posts/PostInfoPage'
 import { PostsPage } from './Posts/PostsPage'
 import { UserInfoPage } from './Users/UserInfoPage'
 import { UsersPage } from './Users/UsersPage'
 
-export { NotFound, PostsPage, PostInfoPage, UsersPage, UserInfoPage }
+export { PostsPage, PostInfoPage, UsersPage, UserInfoPage }
