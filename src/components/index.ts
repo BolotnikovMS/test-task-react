515ca11de7a17680d373c3288b9368dab78398f5
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-23 19:42:05 +0500

    feat: Добавлены импорты

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,13 +5,14 @@ import { FormGroup } from './forms/formGroup/FormGroup'
 import { Header } from './header/Header'
 import { Icon } from './icon/Icon'
 import { InfoMessage } from './infoMessage/InfoMessage'
+import { Input } from './input/Input'
 import { Layout } from './layout/Layout';
 import { Loader } from './loader/Loader'
 import { Pagination } from './pagination/Pagination'
 import { Search } from './search/Search'
 import { SearchList } from './search/SearchList'
-import { TextInput } from './forms/input/TextInput'
+import { TextInput } from './forms/textInput/TextInput'
 import { Textarea } from './forms/textarea/Textarea'
 import { ValidationMessage } from './forms/errorMessage/ValidationMessage'
 
-export { Layout, Header, Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, ValidationMessage, TextInput, Textarea, FormGroup }
+export { Layout, Header, Button, Input, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, ValidationMessage, TextInput, Textarea, FormGroup }
