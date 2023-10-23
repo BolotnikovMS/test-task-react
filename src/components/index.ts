943530544c13d9b9dad2c51fadef9fb90300bfc7
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-23 20:19:25 +0500

    feat: Добавлены импорты

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,15 @@
 import { Button } from './buttons/Button'
 import { Dropdown } from './dropdown/Dropdown'
 import { Error } from './error/Error'
+import { Header } from './header/Header';
 import { Icon } from './icon/Icon'
 import { InfoMessage } from './infoMessage/InfoMessage'
+import { Input } from './input/Input';
+import { Layout } from './layout/layout';
 import { Loader } from './loader/Loader'
 import { Pagination } from './pagination/Pagination'
+import { Router } from './routing/Router'
 import { Search } from './search/Search'
 import { SearchList } from './search/SearchList'
 
-export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList }
+export { Button, Dropdown, Error, Icon, InfoMessage, Loader, Pagination, Search, SearchList, Router, Header, Layout, Input }
